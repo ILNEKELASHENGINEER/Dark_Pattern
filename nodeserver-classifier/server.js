b0b45120f5ff1f13a6f56ef32af6c8950d753786
I.L.NEKELASH <93472619+ILNEKELASHENGINEER@users.noreply.github.com>
2024-02-27 22:39:28 +0530

    selenium + price tracker is integrated

--- nodeserver-classifier/server.js
+++ nodeserver-classifier/server.js
@@ -4,9 +4,12 @@ const path = require('path');
 const Papa = require('papaparse');
 const express = require('express');
 const cors = require('cors');
+const {Builder, By, Key, until} = require('selenium-webdriver');
+const firefox = require('selenium-webdriver/firefox');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 const port = 3000;
 
 const classifier = new natural.BayesClassifier();
@@ -37,6 +40,60 @@ parsedData.data.forEach(data => {
 
 classifier.train();
 
+// app.post('/track', (req, res) => {
+//   const productUrl = req.body.trackData;
+//   // Process the trackData as needed
+//   console.log('Received track data:', trackData);
+
+//   // Send a response back to the client
+//   res.json({ status: 'success', message: 'Track data received successfully' });
+// });
+
+app.post('/track', async (req, res) => {
+  try {
+    const trackData = req.body.trackData;
+    console.log('Received track data:', trackData);
+
+    // Your Selenium code here
+    const seleniumResult = await runSeleniumCode(trackData);
+
+    // Send the Selenium result back to the client
+    res.json({ status: 'success', message: 'Track data received successfully', seleniumResult });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+  }
+});
+
+async function runSeleniumCode(productLink) {
+  let driver;
+
+  try {
+    let options = new firefox.Options();
+    driver = await new Builder()
+      .forBrowser('firefox')
+      .setFirefoxOptions(options)
+      .build();
+
+    await driver.get('https://pricehistory.app/');
+
+    const searchInput = await driver.findElement(By.id('search'));
+    await searchInput.sendKeys(productLink, Key.RETURN);
+
+    const allTimePriceOverview = await driver.wait(until.elementLocated(By.className('all-time-price-overview')), 55000);
+    const text = await allTimePriceOverview.getText();
+
+    const resultArray = text.split('\n');
+    console.log(resultArray);
+    return resultArray;
+  } finally {
+    if (driver) {
+      await driver.quit();
+    }
+  }
+}
+
+
 // API endpoint for classification
 app.get('/classify', (req, res) => {
   const inputText = req.query.text;
@@ -51,5 +108,5 @@ app.get('/classify', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+console.log(`Server running on port ${port}`);
 });
